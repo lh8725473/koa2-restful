63ff809b756d6f7ed7a05d2ede2996e06a950408
liuhao <171213574@qq.com>
2018-10-17 11:34:35 +0800

    文件上传下载

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 const Koa = require('koa')
 const app = new Koa()
-// const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
-const jwtKoa = require('koa-jwt')
 const logger = require('koa-logger')
 const cors = require('koa2-cors')
+const koaBody = require('koa-body')
+const path = require('path')
+const koaStatic = require('koa-static')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
 const company = require('./routes/company')
 
-const secret = 'jwt demo'
-
 // error handler
 onerror(app)
 
 // middlewares
+// 静态资源目录对于相对入口文件app.js的路径
+const staticPath = './public'
+console.log(path.join(__dirname, staticPath))
+app.use(koaStatic(
+  path.join(__dirname, staticPath)
+))
+
+app.use(koaBody({
+  multipart: true,
+  formidable: {
+    maxFileSize: 200 * 1024 * 1024 // 设置上传文件大小最大限制，默认2M
+  }
+}))
 app.use(bodyparser({
   enableTypes: ['json', 'form', 'text']
 }))
@@ -44,21 +56,19 @@ app.use(cors())
 // }))
 
 app.use(logger())
-// app.use(require('koa-static')(__dirname + '/public'))
 
-// app.use(views(__dirname + '/views', {
-//   extension: 'pug'
-// }))
+// console.log('__dirname', __dirname)
+// app.use(views(__dirname + '/public'))
 
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(ctx.body)
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-  ctx.body.times = `${ms}ms`
-})
+// app.use(async (ctx, next) => {
+//   const start = new Date()
+//   await next()
+//   const ms = new Date() - start
+//   console.log(ctx.body)
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+//   // ctx.body.times = `${ms}ms`
+// })
 
 // trans page & pageSize to skip & limit
 app.use(async (ctx, next) => {
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,9 @@ const util = require('util')
 const Joi = require('joi')
 const validate = require('koa2-validation')
 const verify = util.promisify(jwt.verify)
+// const os = require('os')
+const path = require('path')
+const fs = require('fs')
 const secret = 'jwt demo'
 
 const userValidate = {
@@ -49,6 +52,26 @@ router.get('/:_id', userctrl.findUserById)
 router.get('/', validate(userValidate.findList), userctrl.findList)
 router.put('/:_id', validate(userValidate.updateUser), userctrl.updateUser)
 router.put('/own', userctrl.own)
+router.post('/uploadFile', async (ctx, next) => {
+  const file = ctx.request.files.file
+  const reader = fs.createReadStream(file.path)
+  let filePath = path.resolve('public/upload') + `/${file.name}`
+  console.log(filePath)
+  const stream = fs.createWriteStream(filePath)
+  reader.pipe(stream)
+  console.log('uploading %s -> %s', file.name, stream.path)
+  // // 上传单个文件
+  // const file = ctx.request.files.file // 获取上传文件
+  // // 创建可读流
+  // const reader = fs.createReadStream(file.path)
+  // let filePath = path.join(__dirname, 'public/upload') + `/${file.name}`
+  // console.log(filePath)
+  // // 创建可写流
+  // const upStream = fs.createWriteStream(filePath)
+  // // 可读流通过管道写入可写流
+  // reader.pipe(upStream)
+  ctx.body = '上传成功！'
+})
 
 router.get('/userInfo', async (ctx, next) => {
   const token = ctx.header.authorization
